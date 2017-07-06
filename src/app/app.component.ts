edadd1de824ea6a9eba52caa64604507ad3d1d55
Deylon <electron-dl@yandex.ua>
2017-07-06 17:34:03 +0300

    default settings for angular/webpack/ts/node/express

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'root-app',
+    template: `<h2>Angular page</h2>`,
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent { }
